a5e21747e13531c83bb55e8787ee514179e4d0c3
Jeppe Pihl <jpihl08@gmail.com>
2012-04-12 09:47:25 +0200

    safety first\!

--- projects/urls.py
+++ projects/urls.py
@@ -11,14 +11,24 @@ rest_api.register(RunResource())
 urlpatterns = patterns('projects',
 	url(r'^$',                                                                                            'views.home'),
     url(r'^about/$',                                                                                      'views.about'),
+
+    url(r'^user/login/$',                                                                                 'views.log_in'),
+    url(r'^user/logout/$',                                                                                'views.log_out'),
+    url(r'^user/create/$',                                                                                'views.create_user'),
+    url(r'^user/edit/$',                                                                                  'views.edit_user'),
+    url(r'^user/details/$',                                                                               'views.details_user'),
+    url(r'^user/delete/$',                                                                                'views.delete_user'),
+
     url(r'^project/create$',                                                                              'views.create_project'),
     url(r'^project/edit/(?P<project_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',   'views.edit_project'),
     url(r'^project/details/(?P<project_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$','views.details_project'),
     url(r'^project/delete/(?P<project_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$', 'views.delete_project'),
+
     url(r'^test/create/(?P<project_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',    'views.create_test'),
     url(r'^test/edit/(?P<test_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',         'views.edit_test'),
     url(r'^test/details/(?P<test_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',      'views.details_test'),
-    url(r'^test/delete/(?P<test_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',       'views.details_test'),
+    url(r'^test/delete/(?P<test_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',       'views.delete_test'),
+    url(r'^run/delete/(?P<run_id>[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$',         'views.delete_run'),
 )
 
 urlpatterns += patterns('',
--- projects/views.py
+++ projects/views.py
@@ -4,8 +4,10 @@ from models import Project, Test, Run
 from forms import ProjectForm, TestForm
 from django.template import RequestContext
 
-def home(request):
-    return render_to_response('index.html', {'project_list':  Project.objects.all().order_by('name')},context_instance=RequestContext(request))
+from django.contrib.auth import logout, authenticate, login
+
+def home(request, error = None):
+    return render_to_response('index.html', {'project_list':  Project.objects.all().order_by('name'), 'error': error},context_instance=RequestContext(request))
 
 
 def about(request):
@@ -68,7 +70,6 @@ def create_test(request, project_id):
                              
 def edit_test(request, test_id):
     test = get_object_or_404(Test, pk=test_id)
-    print dir(test)
     form = TestForm(request.POST or None)
     if form.is_valid(): # All validation rules pass
         edited_test = form.save()
@@ -85,11 +86,69 @@ def delete_test(request, test_id):
     c = Test.objects.get(pk=test_id)
     project = c.project
     c.delete()
-    #get_object_or_404(Test, pk=test_id).delete()
-    return redirect(project_details, project_id = project.id)
+    return redirect(details_project, project_id = project.key)
 
 def details_test(request, test_id):
     test = get_object_or_404(Test, pk=test_id)
     return render_to_response('test_details.html',
                               {'test': test},
                               context_instance=RequestContext(request))
+
+def delete_run(request, run_id):
+    c = Run.objects.get(pk=run_id)
+    test = c.test
+    c.delete()
+    return redirect(details_test, test_id = test.key)
+
+"""
+USER
+"""
+def create_user(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid(): # All validation rules pass
+        user = form.save()
+        user.save()
+        return redirect(home)
+    return render_to_response('user_constructor.html',
+                              {'errors': form.errors},
+                              context_instance=RequestContext(request))
+
+def edit_user(request, user_id):
+    if request.user.is_authenticated():
+      form = TestForm(request.POST or None)
+      if form.is_valid(): # All validation rules pass
+          edited_test = form.save()
+          test.name = edited_test.name
+          test.description = edited_test.description
+          test.save()
+          return redirect(details_test, test_id=test.key)
+      return render_to_response('test_constructor.html',
+                                {'errors': form.errors,
+                                 'test': test},
+                                context_instance=RequestContext(request))
+    else:
+        return redirect(home)
+
+def log_out(request):
+    logout(request)
+
+def log_in(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            redirect(home)
+        else:
+            return redirect(home)
+            # Return a 'disabled account' error message
+    else:
+        return redirect(home)
+
+
+def delete_user(request, user_id):
+    return redirect(home)
+
+def details_user(request, user_id):
+    return redirect(home)
